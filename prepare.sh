178677b16d872acda860146efa6af4e04ddbe70d
Wladimir David Zakrevskyy <wd.zakrevskyy@icloud.com>
2023-12-28 00:10:18 +0100

    -lib update

--- prepare.sh
+++ prepare.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-mvn clean install  
-mvn org.cyclonedx:cyclonedx-maven-plugin:makeAggregateBom  
-mvn package
+~/java_env/maven/bin/mvn clean install  
+~/java_env/maven/bin/mvn org.cyclonedx:cyclonedx-maven-plugin:makeAggregateBom  
+~/java_env/maven/bin/mvn package
